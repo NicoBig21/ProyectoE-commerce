587f96920121e4b345df8860c51ead9938caf148
NicolasBignone <nicolas.bignone@davinci.edu.ar>
2024-04-11 19:43:30 -0300

    new route

--- src/componentes_pc/routes/ComponentPc.jsx
+++ src/componentes_pc/routes/ComponentPc.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Productos, Detalle, SearchPage } from "../pages";
+import { Productos, Detalle, SearchPage, PageNotFound } from '../pages';
 
 export default function ComponentPc() {
-
     return (
         <Routes>
-            <Route path="search" element={<SearchPage />} />       
+            <Route path="search" element={<SearchPage />} />
             <Route path="productos" element={<Productos />} />
             <Route path="productos/:id" element={<Detalle />} />
+            {/* Ruta comodín para manejar cualquier otra ruta */}
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
     );
-};
-
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import { LoginPage } from '../auth/pages/LoginPage'
-import ComponentPc from "../componentes_pc/routes/ComponentPc";
-import { Home, PageNotFound } from "../componentes_pc/pages";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { LoginPage } from '../auth/pages/LoginPage';
+import ComponentPc from '../componentes_pc/routes/ComponentPc';
+import { Home, PageNotFound } from '../componentes_pc/pages';
 
 export default function AppRouter() {
     return (
         <Routes>
-
-            <Route path="/" element={<Home/>}/>
-
-            <Route path="login" element={<LoginPage/>}/>
-
-            <Route path="componentes-pc/*" element={<ComponentPc/>}/>
-
-            {/* TODO: Buscar como hacer cuando la ruta es "componentes-pc/productos/saajdj" para que aparezca la "PageNotFound" */}
-            <Route path="/*" element={<PageNotFound/>}/>
-
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="componentes-pc/*" element={<ComponentPc />} />
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
-    )
+    );
 }
